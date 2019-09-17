20b30c6717197dd9a803455e3da8dab0428c3597
Simo Aleksandrov <simo3003@me.com>
2019-09-17 21:26:40 +0300

    start working on hasing

--- jahcoin/jahcoin.go
+++ jahcoin/jahcoin.go
@@ -2,8 +2,13 @@ package jahcoin
 
 import (
 	"crypto/ed25519"
+	"crypto/sha256"
+	"encoding/hex"
 	"errors"
+	"fmt"
+	"hash"
 	"math"
+	"math/rand"
 	"sync"
 	"time"
 )
@@ -20,6 +25,7 @@ type Config struct {
 	// Gekyume is the reciever of the first transaction in the genesis block
 	Gekyume                  ed25519.PublicKey
 	InitialTransactionAmount float64
+	Hasher                   hash.Hash
 }
 
 // Blockchain contains all of the blocks and the configuration options
@@ -34,7 +40,6 @@ type Blockchain struct {
 type Block struct {
 	Prev         *Block
 	PrevHash     []byte
-	Hash         []byte
 	Timestamp    time.Time
 	Transactions []Transaction
 	Nonce        int
@@ -46,6 +51,11 @@ type Transaction struct {
 	Amount    float64
 	Recipient ed25519.PublicKey
 	Sender    ed25519.PublicKey
+}
+
+// Hash returns the hash of the block
+func (b *Block) Hash() [sha256.Size]byte {
+	return sha256.Sum256([]byte(fmt.Sprintf("%v", b)))
 }
 
 // NewBlockchain returns a pointer to a blockchain and any errors
@@ -60,15 +70,36 @@ func NewBlockchain(c *Config) (*Blockchain, error) {
 		return nil, ErrInvalidDifficulty
 	}
 
+	t := Transaction{
+		Amount:    c.InitialTransactionAmount,
+		Recipient: c.Gekyume,
+	}
+
+	gekyumeBlock := &Block{
+		Prev:         nil,
+		PrevHash:     nil,
+		Transactions: []Transaction{t},
+	}
+
 	b := &Blockchain{
-		GekyumeBlock: &Block{},
+		GekyumeBlock: gekyumeBlock,
+		CurrentBlock: gekyumeBlock,
 		m:            &sync.Mutex{},
 		Config:       *c,
 	}
 
-	b.CurrentBlock = b.GekyumeBlock
+	b.Mine()
 
 	return b, nil
+}
+
+func (b *Blockchain) Mine() {
+	for {
+		hash := b.CurrentBlock.Hash()
+		fmt.Println(hex.EncodeToString(hash[:]))
+		b.CurrentBlock.Nonce = rand.Int()
+		fmt.Println(b.CurrentBlock.Nonce)
+	}
 }
 
 func (b *Blockchain) AddTransaction(t *Transaction) error {

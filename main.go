a5ba262c13451272279943a01315bec2c89490a8
Simo Aleksandrov <simo3003@me.com>
2019-09-17 20:07:11 +0300

    Generate public and private keys for gekyume

--- main.go
+++ main.go
@@ -1,4 +1,30 @@
 package main
 
+import (
+	"crypto/ed25519"
+	"crypto/rand"
+
+	"github.com/deliriumproducts/jahcoin/jahcoin"
+)
+
 func main() {
+	gekPub, _, err := ed25519.GenerateKey(rand.Reader)
+	if err != nil {
+		panic(err)
+	}
+
+	bc, err := jahcoin.NewBlockchain(&jahcoin.Config{
+		TransactionsPerBlock:     16,
+		Difficulty:               5,
+		Gekyume:                  gekPub,
+		InitialTransactionAmount: 50,
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	bc.AddTransaction(&jahcoin.Transaction{
+		Amount: 999,
+	})
 }

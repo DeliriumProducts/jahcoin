784f80cd65fd9ccebf79f41eaec88a288f72cb3b
Simo Aleksandrov <simo3003@me.com>
2019-09-21 12:28:44 +0300

    make hash func on transaction struct

--- jahcoin/transaction.go
+++ jahcoin/transaction.go
@@ -1,10 +1,24 @@
 package jahcoin
 
-import "crypto/ed25519"
+import (
+	"bytes"
+	"crypto/ed25519"
+	"crypto/sha256"
+	"encoding/gob"
+)
 
 // Transaction is a transaction between 2 parties
 type Transaction struct {
 	Amount    float64
 	Recipient ed25519.PublicKey
 	Sender    ed25519.PublicKey
+}
+
+func (t *Transaction) Hash() ([sha256.Size]byte, error) {
+	bf := &bytes.Buffer{}
+	if err := gob.NewEncoder(bf).Encode(t); err != nil {
+		return [sha256.Size]byte{}, err
+	}
+
+	return sha256.Sum256(bf.Bytes()), nil
 }
